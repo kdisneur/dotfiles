6be34742e31fe532d4ba88ca820142d86e4556dc
Kevin Disneur <kevin@disneur.me>
2016-03-10 15:12:03 +0100

    Improve stag/prod deployment with emacs

--- emacs.d/private/arenaflowers/packages.el
+++ emacs.d/private/arenaflowers/packages.el
@@ -1,5 +1,10 @@
 (setq arenaflowers-packages '(prodigy shell-pop))
 
+(defvar application-path "/Users/work/app")
+(defvar arena-apps-path (concatenate 'string application-path "/arena_apps"))
+(defvar screwcap-path "/Users/work/.rbenv/versions/2.1.6/bin/screwcap")
+(defvar script-root-path (concatenate 'string application-path "/arena_scripts/deployment"))
+
 (with-eval-after-load 'prodigy
   (prodigy-define-tag
     :name 'rails
@@ -13,44 +18,53 @@
     :name "arenaflowers.net:3002"
     :command "bundle"
     :args '("exec" "rails" "server" "-p" "3002")
-    :cwd "/Users/work/app/arena_apps/arenaflowers.net"
+    :cwd (concatenate 'string arena-apps-path "/arenaflowers.net")
     :tags '(arenaflowers rails))
 
   (prodigy-define-service
     :name "arenaflowers.com:3000"
     :command "bundle"
     :args '("exec" "rails" "server" "-p" "3000")
-    :cwd "/Users/work/app/arena_apps/arenaflowers.com"
+    :cwd (concatenate 'string arena-apps-path "/arenaflowers.com")
     :tags '(arenaflowers rails))
 
   (prodigy-define-service
     :name "api.arenaflowers.net:3003"
     :command "bundle"
     :args '("exec" "rails" "server" "-p" "3003")
-    :cwd "/Users/work/app/arena_apps/api.arenaflowers.net"
+    :cwd (concatenate 'string arena-apps-path "/api.arenaflowers.net")
     :tags '(arenaflowers rails))
 
   (prodigy-define-service
     :name "pi.arenaflowers.net:3004"
     :command "bundle"
     :args '("exec" "rails" "server" "-p" "3004")
-    :cwd "/Users/work/app/arena_apps/pi.arenaflowers.net"
+    :cwd (concatenate 'string arena-apps-path "/pi.arenaflowers.net")
     :tags '(arenaflowers rails)))
 
-(defvar cap-projects
-  (list "arenaflowers.com" "partners" "api.arenaflowers.com"))
+(defun cap-command-names (type)
+  (split-string
+   (shell-command-to-string
+    (format "%s %s/%s.rb 2> /dev/null | grep -E '^  ' | sed -e 's/ //g'"
+            screwcap-path
+            script-root-path
+            type)) "\n"))
 
 (defun run-cap-command (command)
   (shell-pop-term 1)
   (comint-send-string "*term-1*" (concatenate 'string command "\n")))
 
-(defun generate-cap-command (type project)
-  (concatenate 'string type "cap " project ":deploy"))
+(defun generate-cap-command (type cap-command)
+  (format "%s %s/%s.rb %s -v\n"
+          screwcap-path
+          script-root-path
+          type
+          cap-command))
 
 (defun helm-cap-search (type)
-  (mapcar (lambda (project-name)
-            (cons project-name (generate-cap-command type project-name)))
-          cap-projects))
+  (mapcar (lambda (cap-command-name)
+            (cons cap-command-name (generate-cap-command type cap-command-name)))
+          (cap-command-names type)))
 
 (defun helm-source-cap-search (type)
   (helm-build-sync-source (concatenate 'string "Deployment to " type)
@@ -61,11 +75,11 @@
 (defun helm-stagcap ()
   "Bring up a stagcap search interface in helm."
   (interactive)
-  (helm :sources (helm-source-cap-search "stag")
+  (helm :sources (helm-source-cap-search "staging")
         :buffer "*helm stagcap*"))
 
 (defun helm-prodcap ()
   "Bring up a prodcap search interface in helm."
   (interactive)
-  (helm :sources (helm-source-cap-search "prod")
+  (helm :sources (helm-source-cap-search "production")
                 :buffer "*helm prodcap*"))
