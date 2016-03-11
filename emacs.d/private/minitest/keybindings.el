1d20914207fdc01f691bd59d862036893533c14e
Kevin Disneur <kevin@disneur.me>
2016-03-11 15:48:40 +0100

    Fix minitest binding to load only in Ruby mode

--- emacs.d/private/minitest/keybindings.el
+++ emacs.d/private/minitest/keybindings.el
@@ -1,2 +1,2 @@
-(spacemacs/set-leader-keys "mmm" 'minitest-verify-single)
-(spacemacs/set-leader-keys "mmf" 'minitest-verify)
+(spacemacs/set-leader-keys-for-major-mode 'enh-ruby-mode "mm" 'minitest-verify-single)
+(spacemacs/set-leader-keys-for-major-mode 'enh-ruby-mode "mf" 'minitest-verify)
