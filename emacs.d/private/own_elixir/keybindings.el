645d317a10d4ec6ed98d48539edb2ac366e0ea09
Kevin Disneur <kevin@disneur.me>
2016-04-24 11:56:11 +0200

    Add some custom spacemacs bindings for Elixir

---
+++ emacs.d/private/own_elixir/keybindings.el
@@ -0,0 +1,10 @@
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "ip" 'own-elixir-inspect-next-pipe)
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "iP" 'own-elixir-inspect-previous-pipe)
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "ir" 'own-elixir-inspect-region)
+
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "td" 'own-elixir-mix-dogma)
+
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "wh" 'own-elixir-wrap-region-with-hash)
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "wl" 'own-elixir-wrap-region-with-list)
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "wp" 'own-elixir-wrap-region-with-parenthesis)
+(spacemacs/set-leader-keys-for-major-mode 'elixir-mode "wt" 'own-elixir-wrap-region-with-tuple)
