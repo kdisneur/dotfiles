70cb4e2916423da21c7245ddde6c8733ff47b79c
Kevin Disneur <kevin@disneur.me>
2016-05-05 21:02:29 +0100

    Update minitest keybinding

--- emacs.d/private/minitest/keybindings.el
+++ emacs.d/private/minitest/keybindings.el
@@ -1,2 +1,3 @@
+(spacemacs/declare-prefix-for-mode 'enh-ruby-mode "m" "minitest")
 (spacemacs/set-leader-keys-for-major-mode 'enh-ruby-mode "mm" 'minitest-verify-single)
-(spacemacs/set-leader-keys-for-major-mode 'enh-ruby-mode "mf" 'minitest-verify)
+(spacemacs/set-leader-keys-for-major-mode 'enh-ruby-mode "mb" 'minitest-verify)
