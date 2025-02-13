952908556913fae3b9d2a71fc23c301ed1a8b0e0
Kevin Disneur <kevin@disneur.me>
2025-02-13 22:05:09 +0100

    Erase stow configuration

--- linux/.bin/exec-once.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-className=${1:?class name is required};
-cmdline=${2:?command to execute};
-if [ -z "$(xdotool search --class "${className}")" ]; then
-  exec ${cmdline}
-fi
--- nvim/.config/nvim/ftplugin/gitcommit.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_option_value('spell', true, { scope = 'local' })
-vim.api.nvim_set_option_value('spelllang', 'en_us', { scope = 'local' })
-vim.api.nvim_set_option_value('textwidth', 72, { scope = 'local' })
